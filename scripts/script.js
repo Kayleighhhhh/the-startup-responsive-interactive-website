66ec586fd498db78ce1b3cfce772704472ed47fd
Kayleighhhhh <kayleigh.vdham04@gmail.com>
2026-01-13 12:54:16 +0100

    header hamburger menu #6 #13

--- scripts/script.js
+++ scripts/script.js
@@ -7,20 +7,20 @@ let maand = vandaag.getMonth()
 
 let emaand = maand + 1
 
-if (emaand < 10) {emaand = "0" + emaand;}
+if (emaand < 10) { emaand = "0" + emaand; }
 
 // console.log(emaand)
 
 let dagen = vandaag.getDate()
 
-if (dagen < 10) {dagen = "0" + dagen;}
+if (dagen < 10) { dagen = "0" + dagen; }
 
 // console.log(dagen)
 
 const datumId = document.getElementById("datumid")
 
 if (datumId) {
-datumId.value = vandaag.getFullYear() + "-" + emaand + "-" + dagen;
+    datumId.value = vandaag.getFullYear() + "-" + emaand + "-" + dagen;
 }
 
 
@@ -29,9 +29,15 @@ const tijdlijn = document.querySelector(".timeline")
 
 // haal de huidige tijd op, (automatisch is dat in seconden sinds 1970)
 
+
+const hours = vandaag.getHours()
+let minutes = vandaag.getMinutes()
+
+if (minutes < 10) { minutes = "0" + minutes; }
+
+console.log(minutes)
+
 if (tijdlijn) {
-    const hours = vandaag.getHours()
-    const minutes = vandaag.getMinutes()
     tijdlijn.style.setProperty('--time', `${hours}.${minutes}`)
 
     // wip scroll to active:
@@ -58,4 +64,19 @@ function playIt() {
     } else {
         audio.pause()
     }
+}
+
+
+// stap 1: zoek de menu-button op en sla die op in een variabele
+const menuBtn = document.querySelector(".btn");
+const deNav = document.querySelector("nav");
+
+// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+menuBtn.addEventListener("click", toggleMenu);
+
+// stap 3: voeg in de functie een class toe aan de nav
+function toggleMenu() {
+    //alert("hoi");
+    deNav.classList.toggle("is-open");
+    menuBtn.classList.toggle("is-open");
 }
